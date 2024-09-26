e1c4a21ad5179c7e0f8965c6c1a8a406746a7df3
yhuang060122 <106745168+yhuang060122@users.noreply.github.com>
2024-09-26 10:02:58 +0200

    first blood

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,37 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DxHttpModule } from 'devextreme-angular/http';
+import { SideNavOuterToolbarModule, SingleCardModule } from './layouts';
+import { FooterModule, ResetPasswordFormModule, CreateAccountFormModule, ChangePasswordFormModule, LoginFormModule } from './shared/components';
+import { AuthService, ScreenService, AppInfoService } from './shared/services';
+import { UnauthenticatedContentModule } from './unauthenticated-content';
+import { AppRoutingModule } from './app-routing.module';
+import { NotAuthorizedContainerModule } from './not-authorized-container';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DxHttpModule,
+    SideNavOuterToolbarModule,
+    SingleCardModule,
+    FooterModule,
+    ResetPasswordFormModule,
+    CreateAccountFormModule,
+    ChangePasswordFormModule,
+    LoginFormModule,
+    UnauthenticatedContentModule,
+    NotAuthorizedContainerModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    ScreenService,
+    AppInfoService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import themes from 'devextreme/ui/themes';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true
-})
-  .catch(err => console.error(err));
+themes.initialized(() => {
+  platformBrowserDynamic()
+    .bootstrapModule(AppModule, {
+      ngZoneEventCoalescing: true,
+    })
+    .catch((err) => console.error(err));
+});
